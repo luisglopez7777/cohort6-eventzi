bc8d7fa72c3933688cc997791a9762da7d85393d
Luis <luisglopez7777@gmail.com>
2020-09-18 00:13:26 -0500

    Backbone completed, ready to push

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import reactDOM from 'react-dom'
+// import './styles/styles.css'
+import './styles/styles.scss'
+
+
+const App = () => {
+    return <h1>Mex</h1>
+}
+
+reactDOM.render(<App />, document.getElementById('root'))
